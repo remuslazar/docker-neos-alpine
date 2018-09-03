88fc033e601e5bbeaaf71b3918d362f417c6ae15
Remus Lazar <remus@lazar.info>
2018-09-03 22:25:46 +0200

    Behat Support
    - configure {Development/Testing}/Behat contexts
    - setup the configuration files
    - use a separate "test db" container for testing

--- root/provision-neos.sh
+++ root/provision-neos.sh
@@ -3,6 +3,9 @@ set -ex
 
 function update_settings_yaml() {
   local settings_file=$1
+  local db_host=$2
+
+  cd /data/www-provisioned
 
   [ -f $settings_file ] || return 0
 
@@ -11,8 +14,32 @@ function update_settings_yaml() {
 	sed -i -r "1,/dbname:/s/dbname: .+?/dbname: \"$DB_DATABASE\"/g" $settings_file
 	sed -i -r "1,/user:/s/user: .+?/user: \"$DB_USER\"/g" $settings_file
 	sed -i -r "1,/password:/s/password: .+?/password: \"$DB_PASS\"/g" $settings_file
-	sed -i -r "1,/host:/s/host: .+?/host: \"$DB_HOST\"/g" $settings_file
+	sed -i -r "1,/host:/s/host: .+?/host: \"$db_host\"/g" $settings_file
 	sed -i -r "1,/port:/s/port: .+?/port: 3306/g" $settings_file
+}
+
+function create_settings_yaml() {
+  local settings_file=$1
+  mkdir -p /data/www-provisioned/$(dirname $settings_file)
+  if [ ! -f /data/www-provisioned/$settings_file ] ; then
+    cp /Settings.yaml /data/www-provisioned/$settings_file
+  fi
+}
+
+function behat_configure_yml_files() {
+  local behat_vhost=$@
+
+  cd /data/www-provisioned
+
+  for f in Packages/*/*/Tests/Behavior/behat.yml.dist; do
+    target_file=${f/.dist/}
+    if [ ! -f $target_file ]; then
+      cp $f $target_file
+    fi
+    # Find all base_url: setting (might be commented out) and replace it with $behat_vhost
+    sed -i -r "s/(#\s?)?base_url:.+/base_url: http:\/\/${behat_vhost}\//g" $target_file
+    echo "$target_file configured for Behat testing."
+  done
 }
 
 # Provision conainer at first run
@@ -20,7 +47,7 @@ if [ -f /data/www/composer.json ] || [ -f /data/www-provisioned/composer.json ] 
 then
 	echo "Do nothing, initial provisioning done"
 	# Update DB Settings to keep them in sync with the docker ENV vars
-	update_settings_yaml /data/www-provisioned/Configuration/Settings.yaml
+	update_settings_yaml Configuration/Settings.yaml $DB_HOST
 else
     # Make sure to init xdebug, not to slow-down composer
     /init-xdebug.sh
@@ -46,12 +73,8 @@ else
     ###
     # Tweak DB connection settings
     ###
-    mkdir -p /data/www-provisioned/Configuration
-		if [ ! -f /data/www-provisioned/Configuration/Settings.yaml ] ; then
-			cp /Settings.yaml /data/www-provisioned/Configuration/
-		fi
-
-		update_settings_yaml /data/www-provisioned/Configuration/Settings.yaml
+    create_settings_yaml Configuration/Settings.yaml
+		update_settings_yaml Configuration/Settings.yaml $DB_HOST
 
     # Set permissions
     chown www-data:www-data -R /tmp/
@@ -65,4 +88,14 @@ else
 			chmod 700 /data/.ssh
 			chmod 600 /data/.ssh/authorized_keys
 	fi
+fi
+
+# create/update Behat Test Setup
+if [ "$DB_TEST_HOST" ] ; then
+  for context in Development/Behat Testing/Behat ; do
+    create_settings_yaml Configuration/$context/Settings.yaml
+    update_settings_yaml Configuration/$context/Settings.yaml $DB_TEST_HOST
+  done
+  behat_configure_yml_files behat.dev.local:${WWW_PORT}
+  chown www-data:www-data -R /data/www-provisioned/Configuration
 fi
